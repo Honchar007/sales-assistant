4aeb2c34ab9f1a452cfbfaa79430442ae422a9f3
Andre <andrey2002335@gmail.com>
2024-02-13 23:12:43 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+      'tsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'indent': [ 'error', 2, { 'SwitchCase': 1 } ],
+    'linebreak-style': 0,
+    'quotes': [ 'error', 'single'],
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'ignore',
+    }],
+    'no-console': 'off',
+    'object-curly-spacing': 'off',
+    'array-bracket-spacing': 'off',
+    'require-jsdoc': 'off',
+    'max-len': [
+      1,
+      120,
+      4,
+      { ignoreComments: true, ignoreUrls: true, ignoreStrings: true },
+    ],
+    'camelcase': 'off',
+    'new-cap': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+    ],
+    'guard-for-in': 'off',
+    'react/display-name': 'off',
+  },
+};
