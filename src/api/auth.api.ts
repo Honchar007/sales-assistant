3c063d2edbbf064cf0985f13d3d040b35005953f
Andre <andrey2002335@gmail.com>
2024-02-15 17:04:32 +0200

    write auth api

---
+++ src/api/auth.api.ts
@@ -0,0 +1,55 @@
+// models
+import { IAccessDTO } from '../submodules/public-common/interfaces/dto/auth/iaccess.interface';
+import { IAccountResponseDTO } from '../submodules/public-common/interfaces/dto/account/iaccount-response.interfaces';
+
+
+const AuthApi = {
+  async authUser(email: string, password: string): Promise<IAccessDTO> {
+    const response = await fetch(`${process.env.API_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to authenticate user');
+    }
+
+    return response.json();
+  },
+
+  async refreshToken(token: string): Promise<IAccessDTO> {
+    const response = await fetch(`${process.env.API_URL}/auth/token/refresh`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to refresh token');
+    }
+
+    return response.json();
+  },
+
+  async recoverUser(token: string): Promise<IAccountResponseDTO> {
+    const response = await fetch(`${process.env.API_URL}/auth/recover-user}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to recover user');
+    }
+
+    return response.json();
+  },
+};
+
+export default AuthApi;
